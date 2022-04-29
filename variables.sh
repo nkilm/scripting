385f585b22a6ea6e9bc5c3d7c694329c3b35395b
nkilm <nikhilmohitelhs@gmail.com>
2022-04-29 23:05:48 +0530

    variables

---
+++ variables.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/bash
+
+echo "Variables in Shell Scripting"
+
+echo $BASH
+echo Bash Version is $BASH_VERSION
+# echo $HOME
+# echo $PWD # present working directory 
+
+echo "Home: $HOME"
