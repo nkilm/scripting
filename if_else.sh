deee916be0608b87555caa3bc3db1a07d2fe3515
nkilm <nikhilmohitelhs@gmail.com>
2022-05-01 00:45:37 +0530

    if-else

---
+++ if_else.sh
@@ -0,0 +1,34 @@
+#! /usr/bin/bash
+
+# operators 
+
+# -eq
+# -ne
+# -lt
+# -gt
+
+
+# if [condition]
+# then
+#     statements
+# fi 
+
+# read -p "Enter your age: " AGE 
+
+# if [ $AGE -gt 18 ] 
+# then 
+#     echo "You can Drive"
+# else
+#     echo "Wait until you are above 18"
+# fi
+
+# for strings 
+
+str="foobarbaz"
+
+if [[ $str == "foobarbaz" ]]
+then 
+    echo "Happy Hacking!🍻"
+else 
+    echo "Try again"
+fi
