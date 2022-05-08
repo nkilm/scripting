d03edcb08f056cbd86f61cc08e24473fab0e4534
nkilm <nikhilmohitelhs@gmail.com>
2022-05-08 11:34:18 +0530

    find and replace in files

---
+++ files/find_and_replace.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+
+# sed -i "s/<pattern>/<replacement>/" file_name # use /g in the end to replace more than one occurence
+
+sed -i "s/mister/ser/" sample.txt
+# sed -i 's/toreplace/yoursetting/' configfile 
