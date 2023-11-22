8cd8e47af598e1398d258e9f5839e44bf63c87fb
Nikhil Mohite <hello@nikhilmohite.com>
2023-11-22 19:34:19 +0530

    update script to run on all laptops

--- scripts/default_web_cam.sh
+++ scripts/default_web_cam.sh
@@ -3,15 +3,20 @@
 # Path to the udev rules file
 udev_rules_file="/etc/udev/rules.d/40-disable-internal-webcam.rules"
 
-# Webcam details
-idVendor="0c45"
-idProduct="671e"
-
 disable_webcam() {
     echo "Disabling the internal webcam..."
-    echo "ATTRS{idVendor}==\"$idVendor\", ATTRS{idProduct}==\"$idProduct\", RUN=\"/bin/sh -c 'echo 0 >/sys/\$devpath/authorized'\"" | sudo tee "$udev_rules_file" > /dev/null
-    sudo udevadm control --reload-rules
-    echo "Webcam disabled. Reboot your system to apply the changes."
+    webcam_info=$(lsusb | grep 'Integrated_Webcam_HD')
+    idVendor=$(echo "$webcam_info" | awk '{print $6}' | cut -d ':' -f1)
+    idProduct=$(echo "$webcam_info" | awk '{print $6}' | cut -d ':' -f2)
+
+    if [ -n "$webcam_info" ]; then
+        echo "ATTRS{idVendor}==\"$idVendor\", ATTRS{idProduct}==\"$idProduct\", RUN=\"/bin/sh -c 'echo 0 >/sys/\$devpath/authorized'\"" | sudo tee "$udev_rules_file" >/dev/null
+        sudo udevadm control --reload-rules
+        echo "Webcam disabled. Reboot your system to apply the changes."
+    else
+        echo "Error: Integrated webcam not found."
+        exit 1
+    fi
 }
 
 enable_webcam() {
@@ -23,16 +28,16 @@ enable_webcam() {
 
 # Check for the script arguments
 case "$1" in
-    "disable")
-        disable_webcam
-        ;;
-    "enable")
-        enable_webcam
-        ;;
-    *)
-        echo "Usage: $0 {disable|enable}"
-        exit 1
-        ;;
+"disable")
+    disable_webcam
+    ;;
+"enable")
+    enable_webcam
+    ;;
+*)
+    echo "Usage: $0 {disable|enable}"
+    exit 1
+    ;;
 esac
 
 exit 0
