6feabaa3e615d70b5114b0cf6251625ff857a8f8
Nikhil Mohite <hello@nikhilmohite.com>
2024-10-27 22:11:15 +0530

    hello world script

---
+++ tutorial/hello-world.sh
@@ -0,0 +1,6 @@
+# Execute script files from remote URL's
+# https://raw.githubusercontent.com/nkilm/scripting/refs/heads/main/tutorial/hello-world.sh
+
+# TLDR;
+# wget -O - <link> | bash
+echo "hello world"
